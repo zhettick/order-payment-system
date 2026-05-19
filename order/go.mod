c34b187977f91915ce3d7b12e0e854c1f5e05be6
zhettick <belikeotall@gmail.com>
2026-05-19 07:33:41 +0500

    feat(order): implement Redis order cache

--- order/go.mod
+++ order/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.12.0
 	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.12.1
+	github.com/redis/go-redis/v9 v9.19.0
 	github.com/zhettick/order-payment-gen v1.1.2
 	google.golang.org/grpc v1.80.0
 	google.golang.org/protobuf v1.36.11
@@ -15,6 +16,7 @@ require (
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic v1.15.0 // indirect
 	github.com/bytedance/sonic/loader v0.5.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.12 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
@@ -35,6 +37,7 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.1 // indirect
 	go.mongodb.org/mongo-driver/v2 v2.5.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/arch v0.22.0 // indirect
 	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/net v0.51.0 // indirect
--- order/internal/cache/redis/order_cache.go
+++ order/internal/cache/redis/order_cache.go
@@ -1,1 +1,62 @@
 package redis
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"order/internal/domain/entities"
+	"time"
+
+	goredis "github.com/redis/go-redis/v9"
+)
+
+type OrderCache struct {
+	client *goredis.Client
+	ttl    time.Duration
+}
+
+func NewOrderCache(client *goredis.Client, ttl time.Duration) *OrderCache {
+	return &OrderCache{
+		client: client,
+		ttl:    ttl,
+	}
+}
+
+func (c *OrderCache) Get(ctx context.Context, id string) (*entities.Order, error) {
+	key := c.key(id)
+
+	value, err := c.client.Get(ctx, key).Result()
+	if errors.Is(err, goredis.Nil) {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	var order entities.Order
+	if err := json.Unmarshal([]byte(value), &order); err != nil {
+		return nil, err
+	}
+
+	return &order, nil
+}
+
+func (c *OrderCache) Set(ctx context.Context, order *entities.Order) error {
+	key := c.key(order.ID)
+
+	data, err := json.Marshal(order)
+	if err != nil {
+		return err
+	}
+
+	return c.client.Set(ctx, key, data, c.ttl).Err()
+}
+
+func (c *OrderCache) Delete(ctx context.Context, id string) error {
+	return c.client.Del(ctx, c.key(id)).Err()
+}
+
+func (c *OrderCache) key(id string) string {
+	return fmt.Sprintf("order:%s", id)
+}
