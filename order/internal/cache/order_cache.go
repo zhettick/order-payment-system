164196c37cd4264d47f4bfde8681cab4fca78ce1
zhettick <belikeotall@gmail.com>
2026-05-19 07:14:20 +0500

    feat(order): add Redis cache client

---
+++ order/internal/cache/order_cache.go
@@ -0,0 +1,12 @@
+package cache
+
+import (
+	"context"
+	"order/internal/domain/entities"
+)
+
+type OrderCache interface {
+	Get(ctx context.Context, id string) (*entities.Order, error)
+	Set(ctx context.Context, order *entities.Order) error
+	Delete(ctx context.Context, id string) error
+}
